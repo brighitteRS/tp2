26630d9a868fd6776ed208decd72f60ffea9eec3
Ignacio Cettour <ICettour@fi.uba.ar>
2026-06-21 21:18:13 -0300

    Update Mafioso.java

--- src/main/java/edu/fiuba/algo3/modelo/Roles/Mafioso.java
+++ src/main/java/edu/fiuba/algo3/modelo/Roles/Mafioso.java
@@ -4,19 +4,32 @@ import edu.fiuba.algo3.modelo.*;
 import edu.fiuba.algo3.modelo.NullPattern.BandoNulo;
 import edu.fiuba.algo3.modelo.NullPattern.JugadorNulo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Mafioso extends Rol {
 
+    private List<Jugador> complices = new ArrayList<>();
     private Jugador victimaElegida = new JugadorNulo();
 
     public Mafioso() {
         super(BandoMafia.INSTANCIA);
     }
 
+    //usarlo en partida
+    public void reconocerComplices(List<Jugador> complices) {
+        this.complices = complices;
+    }
+
+    //usarlo para los test
+    public boolean esComplice(Jugador jugador){
+        return complices.contains(jugador);
+    }
 
     @Override
     public Bando revelarBandoA(Jugador solicitante) {
 
-        if (solicitante.esDeLaMafia()) {
+        if (solicitante.esDeLaMafia()) { //usar escomplice
             return revelarBando();}
 
         return BandoNulo.INSTANCIA;
